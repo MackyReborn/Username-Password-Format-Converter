10701d7e1bc2b8e746a318a57cb28bfadaa6db44
Macky <49353641+MackyReborn@users.noreply.github.com>
2020-01-01 20:40:16 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Username:Password Converter
+ Converts username password format to username:password so they can easily be added to a database. It appends this to a txt file called "Accounts.txt"
---
+++ Source/main.py
@@ -0,0 +1,18 @@
+def writeToFile(account):
+    theFile = open("accounts.txt", "a")
+    theFile.write(account)
+    theFile.write('\n')
+
+def convert(username, password):
+    result = username + ":" + password
+    return result
+
+while True:
+    usr = input("Enter Username: ")
+    psswrd = input("Enter Password: ")
+
+    if usr == 'x':
+        break
+
+    writeToFile(convert(usr, psswrd))
+    print("Done\n")
